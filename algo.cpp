694b21e650840164eb9dae3cae426e558ea0b326
monjara <yaginumee@gmail.com>
2021-06-07 16:11:55 +0900

    190-B

--- algo.cpp
+++ algo.cpp
@@ -2,8 +2,17 @@
 using namespace std;
 
 int main(){
-    int a, b, c;
-    cin >> a >> b >> c;
-    a + c > b ? cout << "Takahashi" << endl : cout << "Aoki" << endl;
+    int N, S, D;
+    cin >> N >> S >> D;
+    vector<int> A(N);
+    vector<int> B(N);
+    for (int i = 0; i < N; i++) {
+        cin >> A.at(i) >> B.at(i);
+    }
+    bool dmg_flg = false;
+    for (int i = 0; i < N; i++) {
+        if (S > A.at(i) && D < B.at(i)) dmg_flg = true;
+    }
+    dmg_flg ? cout << "Yes" << endl : cout << "No" << endl;
     return 0;
 }
