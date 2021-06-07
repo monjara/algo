2d0085b666744f132190cdc4d1b7dfc0c181771d
monjara <yaginumee@gmail.com>
2021-06-07 16:04:27 +0900

    190-A

--- algo.cpp
+++ algo.cpp
@@ -2,19 +2,8 @@
 using namespace std;
 
 int main(){
-    int N;
-    cin >> N;
-    vector<int> A(N);
-    vector<int> B(N);
-    for (int i = 0; i < N; i++) {
-        cin >> A.at(i) >> B.at(i);
-    }
-    int ans = 1000000;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            ans = min(ans, i == j ? A.at(i) + B.at(j) : max(A.at(i), B.at(j)));
-        }
-    }
-    cout << ans << endl;
+    int a, b, c;
+    cin >> a >> b >> c;
+    a + c > b ? cout << "Takahashi" << endl : cout << "Aoki" << endl;
     return 0;
 }
