50819a7628e838158421072b1586ddfef765aa38
monjara <yaginumee@gmail.com>
2021-06-07 15:47:23 +0900

    194-B

--- algo.cpp
+++ algo.cpp
@@ -2,14 +2,19 @@
 using namespace std;
 
 int main(){
-    int A, B;
-    cin >> A >> B;
-    int kokei = A + B;
-    int ans;
-    if (kokei >= 15 && B >= 8) ans = 1;
-    else if (kokei >= 10 && B >= 3) ans = 2;
-    else if (kokei >= 3) ans = 3;
-    else ans = 4;
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    vector<int> B(N);
+    for (int i = 0; i < N; i++) {
+        cin >> A.at(i) >> B.at(i);
+    }
+    int ans = 1000000;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            ans = min(ans, i == j ? A.at(i) + B.at(j) : max(A.at(i), B.at(j)));
+        }
+    }
     cout << ans << endl;
     return 0;
 }
