da0f78dced896ccae97fce0dff5dede222ffd847
monjara <yaginumee@gmail.com>
2021-06-14 08:56:53 +0900

    1_1_D

--- aoj-pra/aoj.cpp
+++ aoj-pra/aoj.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 
 int main() {
-	int a, b;
-	cin >> a >> b;
-	int area = a * b;
-	int length = (a + b) * 2;
-	cout << area << " " << length << endl;
+	int S;
+	cin >> S;
+	int h = S / 3600;
+	int m = (S / 60) % 60;
+	int s = S % 60;
+	cout << h << ":" << m << ":" << s << endl;
 	return 0;
 }
