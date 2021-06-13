eae410c4cdd1c39fb4237e4a2f570516d1c0027c
monjara <yaginumee@gmail.com>
2021-06-14 08:39:04 +0900

    1_1_C

--- aoj-pra/aoj.cpp
+++ aoj-pra/aoj.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
-#include<cmath>
 using namespace std;
 
 int main() {
-	int x;
-	cin >> x;
-	int out = pow(x, 3);
-	cout << out << endl;
+	int a, b;
+	cin >> a >> b;
+	int area = a * b;
+	int length = (a + b) * 2;
+	cout << area << " " << length << endl;
 	return 0;
 }
