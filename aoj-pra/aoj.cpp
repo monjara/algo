ac05cb462df9178d563b5e1bf438fc10552009ab
monjara <yaginumee@gmail.com>
2021-06-14 09:23:53 +0900

    1_2_C

--- aoj-pra/aoj.cpp
+++ aoj-pra/aoj.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
 int main() {
-	int a, b, c;
-	cin >> a >> b >> c;
-	if (a < b && b < c) cout << "Yes" << endl;
-	else cout << "No" << endl;
+	vector<int> a(3);
+	cin >> a.at(0) >> a.at(1) >> a.at(2);
+	sort(a.begin(), a.end());
+	cout << a.at(0) << " " << a.at(1) << " " << a.at(2) << endl;
 	return 0;
 }
