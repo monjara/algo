c92a786ab66ba8e6e94aaa12d427dcfd5b7792a7
monjara <yaginumee@gmail.com>
2021-06-14 13:59:36 +0900

    1_3_A

--- aoj-pra/aoj.cpp
+++ aoj-pra/aoj.cpp
@@ -2,12 +2,6 @@
 using namespace std;
 
 int main() {
-	int w, h, x, y, r;
-	cin >> w >> h >> x >> y >> r;
-	if (x - r >= 0 && y - r >= 0) {
-		if (w >= x + r && h >= y + r) cout << "Yes" << endl;
-		else cout << "No" << endl;
-	}
-	else cout << "No" << endl;
+	for (int i = 0; i < 1000; i++) cout << "Hello World" << endl;
 	return 0;
 }
