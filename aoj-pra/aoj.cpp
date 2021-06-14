85c635d6c874b3fb9375120ddb3374542409cd1f
monjara <yaginumee@gmail.com>
2021-06-14 16:17:25 +0900

    1_6_A

--- aoj-pra/aoj.cpp
+++ aoj-pra/aoj.cpp
@@ -1,22 +1,16 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int main() {
 	int n;
 	cin >> n;
-	for (int i = 1; i <= n; i++) {
-		int x = i / 10;
-		if (i % 3 == 0) cout << " " << i;
-		else {
-			int x = i;
-			do {
-				if (x % 10 == 3) {
-					cout << " " << i;
-					break;
-				}
-				x /= 10;
-			} while (x);
-		}
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a.at(i);
+	}
+	for (int i = n - 1; i >= 0; i--) {
+		i == 0 ? cout << a.at(i) : cout << a.at(i) << " ";
 	}
 	cout << endl;
 	return 0;
