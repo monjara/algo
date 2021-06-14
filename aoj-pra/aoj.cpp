a45f8d4d761ddce3bc83ec53aba027b365e3b301
monjara <yaginumee@gmail.com>
2021-06-14 15:37:47 +0900

    1_5_B

--- aoj-pra/aoj.cpp
+++ aoj-pra/aoj.cpp
@@ -8,7 +8,9 @@ int main() {
 		if (h == 0 && w == 0) return 0;
 		for (int i = 0; i < h; i++) {
 			for (int j = 0; j < w; j++) {
-				cout << "#";
+				if (i == 0 || i == h - 1) cout << "#";
+				else if (j == 0 || j == w - 1) cout << "#";
+				else cout << ".";
 			}
 			cout << endl;
 		}
