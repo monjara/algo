6a3bdae21ea0317ab904644b336eaa08e40a1915
monjara <yaginumee@gmail.com>
2021-06-14 14:09:04 +0900

    1_3_B

--- aoj-pra/aoj.cpp
+++ aoj-pra/aoj.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 int main() {
-	for (int i = 0; i < 1000; i++) cout << "Hello World" << endl;
+	for (int i = 0; i < 10000; i++) {
+		int x;
+		cin >> x;
+		if (x == 0) return 0;
+		cout << "Case " << i+1 << ": " << x << endl;
+	}
 	return 0;
 }
