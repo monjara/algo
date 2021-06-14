04294a120ed0e892dd87868542eced92b9972b37
monjara <yaginumee@gmail.com>
2021-06-14 22:41:43 +0900

    1_6_D

--- aoj-pra/aoj.cpp
+++ aoj-pra/aoj.cpp
@@ -3,54 +3,24 @@
 using namespace std;
 
 int main() {
-	int n;
-	cin >> n;
-	vector<vector<int>> one(3, vector<int>(10));
-	vector<vector<int>> two(3, vector<int>(10));
-	vector<vector<int>> three(3, vector<int>(10));
-	vector<vector<int>> four(3, vector<int>(10));
+	int n, m;
+	cin >> n >> m;
+	vector<vector<int>> a(n, vector<int>(m));
+	vector<int> b(m);
 	for (int i = 0; i < n; i++) {
-		int b, f, r, v;
-		cin >> b >> f >> r >> v;
-		if (b == 1) {
-			one.at(f - 1).at(r - 1) += v;
-		}
-		else if (b == 2) {
-			two.at(f - 1).at(r - 1) += v;
-		}
-		else if (b == 3) {
-			three.at(f - 1).at(r - 1) += v;
-		}
-		else if (b == 4) {
-			four.at(f - 1).at(r - 1) += v;
+		for (int j = 0; j < m; j++) {
+			cin >> a.at(i).at(j);
 		}
 	}
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 10; j++) {
-			cout << " " << one.at(i).at(j);
-		}
-		cout << endl;
+	for (int i = 0; i < m; i++) {
+		cin >> b.at(i);
 	}
-	cout << "####################" << endl;
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 10; j++) {
-			cout << " " << two.at(i).at(j);
+	for (int i = 0; i < n; i++) {
+		int out = 0;
+		for (int j = 0; j < m; j++) {
+			out += b.at(j) * a.at(i).at(j);
 		}
-		cout << endl;
-	}
-	cout << "####################" << endl;
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 10; j++) {
-			cout << " " << three.at(i).at(j);
-		}
-		cout << endl;
-	}
-	cout << "####################" << endl;
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 10; j++) {
-			cout << " " << four.at(i).at(j);
-		}
-		cout << endl;
+		cout << out << endl;
 	}
 	return 0;
 }
