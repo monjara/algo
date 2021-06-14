4879a1704ca2a064227e6e20edbacd48e7c3af91
monjara <yaginumee@gmail.com>
2021-06-14 16:10:50 +0900

    1_5_D

--- aoj-pra/aoj.cpp
+++ aoj-pra/aoj.cpp
@@ -2,21 +2,22 @@
 using namespace std;
 
 int main() {
-	for (int i = 0; i < 1000000; i++) {
-		int h, w;
-		cin >> h >> w;
-		if (h == 0 && w == 0) return 0;
-		for (int i = 1; i <= h; i++) {
-			for (int j = 1; j <= w; j++) {
-				if (i % 2 == 1) {
-					j % 2 == 1 ? cout << "#" : cout << ".";
-				} else {
-					j % 2 == 1 ? cout << "." : cout << "#";
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		int x = i / 10;
+		if (i % 3 == 0) cout << " " << i;
+		else {
+			int x = i;
+			do {
+				if (x % 10 == 3) {
+					cout << " " << i;
+					break;
 				}
-			}
-			cout << endl;
+				x /= 10;
+			} while (x);
 		}
-		cout << endl;
 	}
+	cout << endl;
 	return 0;
 }
