927f78aaa0c7461f883504869743726bd6bf8cee
monjara <yaginumee@gmail.com>
2021-06-14 23:20:09 +0900

    1_7_B

--- aoj-pra/aoj.cpp
+++ aoj-pra/aoj.cpp
@@ -3,22 +3,19 @@
 using namespace std;
 
 int main() {
-	for (int i = 0; i < 51; i++) {
-		int m, f, r;
-		cin >> m >> f >> r;
-		if (m == -1 && f == -1 && r == -1) return 0;
-		char out;
-		if (m == -1 || f == -1) out = 'F';
-		else if (m + f >= 80) out = 'A';
-		else if (m + f >= 65) out = 'B';
-		else if (m + f >= 50) out = 'C';
-		else if (m + f >= 30) {
-			if (r >= 50) out = 'C';
-			else out = 'D';
+	for (int i = 0; i < 1000001; i++) {
+		int n, x;
+		cin >> n >> x;
+		if (n == 0 && x == 0) return 0;
+		int count = 0;
+		for (int i = 1; i <= n; i++) {
+			for (int j = i + 1; j <= n; j++) {
+				for (int k = j + 1; k <= n; k++) {
+					if (i + j + k == x) count++;
+				}
+			}
 		}
-		else if (m + f < 30) out = 'F';
-
-        cout << out << endl;
+		cout << count << endl;
 	}
 	return 0;
 }
