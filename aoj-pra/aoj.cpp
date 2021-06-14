d8b8777a603202789c491e9a981268d980ce4640
monjara <yaginumee@gmail.com>
2021-06-14 14:56:25 +0900

    1_4_B

--- aoj-pra/aoj.cpp
+++ aoj-pra/aoj.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 int main() {
-	double a, b;
-	cin >> a >> b;
-	int d = a / b;
-	int r = (int)fmod(a, b);
-	double f = a / b;
-	cout << d << " " << r << " " << fixed << setprecision(8) << f << endl;
+	double pi = 3.14159265359;
+	double r;
+	cin >> r;
+	double a = pi * r * r;
+	double l = 2 * pi * r;
+	cout << fixed << setprecision(6) << a << " " << l << endl;
 	return 0;
 }
