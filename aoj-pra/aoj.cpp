ba513b3a0d7fca8f3c5837ac9b848b68322ce41e
monjara <yaginumee@gmail.com>
2021-06-15 15:06:52 +0900

    1_8_A

--- aoj-pra/aoj.cpp
+++ aoj-pra/aoj.cpp
@@ -1,36 +1,12 @@
 #include<iostream>
-#include<vector>
-#include<numeric>
 using namespace std;
 
 int main() {
-	int n, m, l;
-	cin >> n >> m >> l;
-	vector<vector<int>> a(n, vector<int>(m));
-	vector<vector<int>> b(m, vector<int>(l));
-	vector<vector<int>> c(n, vector<int>(l));
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> a.at(i).at(j);
-		}
+	string s;
+	getline(cin, s);
+	for (int i = 0; i < s.size(); i++) {
+		s[i] = isupper(s[i]) ? tolower(s[i]) : toupper(s[i]);
 	}
-	for (int i = 0; i < m; i++) {
-		for (int j = 0; j < l; j++) {
-			cin >> b.at(i).at(j);
-		}
-	}
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < l; j++) {
-			for (int k = 0; k < m; k++) {
-				c.at(i).at(j) += a.at(i).at(j) * b.at(j).at(k);
-			}
-		}
-	}
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < l; j++) {
-			cout << c.at(i).at(j) << " ";
-		}
-		cout << endl;
-	}
+	cout << s << endl;
 	return 0;
 }
