0c8d3c1d772cead4bb3cf5c0df21498befc5293d
monjara <yaginumee@gmail.com>
2021-06-15 13:43:46 +0900

    1_7_C

--- aoj-pra/aoj.cpp
+++ aoj-pra/aoj.cpp
@@ -1,21 +1,30 @@
 #include<iostream>
 #include<vector>
+#include<numeric>
 using namespace std;
 
 int main() {
-	for (int i = 0; i < 1000001; i++) {
-		int n, x;
-		cin >> n >> x;
-		if (n == 0 && x == 0) return 0;
-		int count = 0;
-		for (int i = 1; i <= n; i++) {
-			for (int j = i + 1; j <= n; j++) {
-				for (int k = j + 1; k <= n; k++) {
-					if (i + j + k == x) count++;
-				}
-			}
+	int r, c;
+	cin >> r >> c;
+	vector<vector<int>> a(r + 1, vector<int>(c + 1));
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			cin >> a.at(i).at(j);
 		}
-		cout << count << endl;
+	}
+	for (int i = 0; i < r; i++) {
+		a.at(i).at(c) = accumulate(a.at(i).begin(), a.at(i).end(), 0);
+	}
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c + 1; j++) {
+			a.at(r).at(j) += a.at(i).at(j);
+		}
+	}
+	for (int i = 0; i < r + 1; i++) {
+		for (int j = 0; j < c + 1; j++) {
+			cout << a.at(i).at(j) << " ";
+		}
+		cout << endl;
 	}
 	return 0;
 }
