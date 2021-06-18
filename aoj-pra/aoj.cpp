eaeabf44a2f2ef7457b076704a6c02bc5fddf9b9
monjara <yaginumee@gmail.com>
2021-06-18 16:17:40 +0900

    一旦コミット

--- aoj-pra/aoj.cpp
+++ aoj-pra/aoj.cpp
@@ -2,15 +2,5 @@
 using namespace std;
 
 int main() {
-	for (int i = 0; i < 1000001; i++) {
-		string s;
-		cin >> s;
-		if (s == "0") return 0;
-		int sum = 0;
-		for (int j = 0; j < s.size(); j++) {
-			sum += s[j] - '0';
-		}
-		cout << sum << endl;
-	}
 	return 0;
 }
