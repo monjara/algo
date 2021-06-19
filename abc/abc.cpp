c9c4ac834220ae3353c7582198289413e777d84a
monjara <yaginumee@gmail.com>
2021-06-19 23:15:58 +0900

    200-B

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -3,13 +3,15 @@
 using namespace std;
 
 int main() {
-	int n, x;
-	cin >> n >> x;
-	for (int i = 0; i < n; i++) {
-		int a;
-		cin >> a;
-		if (x != a) cout << a << " ";
+	long long n, k;
+	cin >> n >> k;
+	for (int i = 0; i < k; i++) {
+		if (n % 200 == 0) {
+			n = n / 200;
+		} else {
+			n = n * 1000 + 200;
+		}
 	}
-	cout << endl;
+	cout << n << endl;
 	return 0;
 }
