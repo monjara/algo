4b6c2bbbbab9cb2e0318f235a41e47a1de557780
monjara <yaginumee@gmail.com>
2021-06-20 16:38:15 +0900

    199-B

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -3,9 +3,21 @@
 using namespace std;
 
 int main() {
-	int a, b, c;
-	cin >> a >> b >> c;
-	if (pow(a, 2) + pow(b, 2) < pow(c, 2)) cout << "Yes" << endl;
-	else cout << "No";
+	int n;
+	cin >> n;
+	int min_tmp = 1000;
+	int max_tmp = 0;
+	for (int i = 0; i < n; i++) {
+		int tmp;
+		cin >> tmp;
+		max_tmp = max(max_tmp, tmp);
+	}
+	for (int i = 0; i < n; i++) {
+		int tmp;
+		cin >> tmp;
+		min_tmp = min(min_tmp, tmp);
+	}
+	int res = min_tmp - max_tmp + 1;
+	(res > 0) ? cout << res << endl : cout << 0 << endl;
 	return 0;
 }
