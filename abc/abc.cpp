7c0f7f3d3136cdd810a25d22ada7a68272740eb8
monjara <yaginumee@gmail.com>
2021-06-23 01:06:53 +0900

    192-A

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -3,21 +3,9 @@
 using namespace std;
 
 int main() {
-	int n;
-	cin >> n;
-	int min_tmp = 1000;
-	int max_tmp = 0;
-	for (int i = 0; i < n; i++) {
-		int tmp;
-		cin >> tmp;
-		max_tmp = max(max_tmp, tmp);
-	}
-	for (int i = 0; i < n; i++) {
-		int tmp;
-		cin >> tmp;
-		min_tmp = min(min_tmp, tmp);
-	}
-	int res = min_tmp - max_tmp + 1;
-	(res > 0) ? cout << res << endl : cout << 0 << endl;
+	int x;
+	cin >> x;
+	int res = 100 - (x % 100);
+	cout << res << endl;
 	return 0;
 }
