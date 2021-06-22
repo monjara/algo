f29eb5c125c1d6a656d5bb6d1b3d6cbd9ec2cf76
monjara <yaginumee@gmail.com>
2021-06-23 01:49:51 +0900

    193-B

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -3,9 +3,24 @@
 using namespace std;
 
 int main() {
-	double a, b;
-	cin >> a >> b;
-	double res = (a - b) / a * 100;
-	cout << res << endl;
+	int n;
+	cin >> n;
+	vector<int> outs(n);
+	bool can = false;
+	int res = 1000000001;
+
+	for (int i = 0; i < n; i++) {
+		int a, p, x;
+		cin >> a >> p >> x;
+		if (x - a > 0) {
+			can = true;
+			outs.at(i) = p;
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		if (outs.at(i) > 0) res = min(res, outs.at(i));
+	}
+	if (can) cout << res << endl;
+	else cout << -1 << endl;
 	return 0;
 }
