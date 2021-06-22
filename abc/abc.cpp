c264bc88d6d4669aa644878f5cb8f0cb801a995b
monjara <yaginumee@gmail.com>
2021-06-23 01:32:46 +0900

    193-A

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -3,14 +3,9 @@
 using namespace std;
 
 int main() {
-	string 	s;
-	cin >> s;
-	bool flg = false;
-	for (int i = 0; i < s.size(); i++) {
-		if (i % 2 == 1 && islower(s[i])) flg = true;
-		else if (i % 2 == 0 && isupper(s[i])) flg = true;
-	}
-	if (flg) cout << "No" << endl;
-	else cout << "Yes" << endl;
+	double a, b;
+	cin >> a >> b;
+	double res = (a - b) / a * 100;
+	cout << res << endl;
 	return 0;
 }
