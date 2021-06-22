14ef855d6103369c3fa37c6c30e2606c881dbf73
monjara <yaginumee@gmail.com>
2021-06-23 03:02:03 +0900

    182-B

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -3,9 +3,24 @@
 using namespace std;
 
 int main() {
-	int a, b;
-	cin >> a >> b;
-	int res = a * 2 + 100 - b;
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a.at(i);
+	}
+	int res = 0;
+	int max = 0;
+	for (int i = 2; i <= 1000; i++) {
+		int count = 0;
+		for (int j = 0; j < n; j++) {
+			if (a.at(j) % i == 0) count++;
+		}
+		if (max < count) {
+			max = count;
+			res = i;
+		}
+	}
 	cout << res << endl;
 	return 0;
 }
