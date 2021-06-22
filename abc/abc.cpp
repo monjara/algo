d8b090694a9e6cc54058b2e8e654a9d7c3f60a6b
monjara <yaginumee@gmail.com>
2021-06-23 02:26:23 +0900

    188-B

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -3,9 +3,21 @@
 using namespace std;
 
 int main() {
-	int x, y;
-	cin >> x >> y;
-	if (x > y) x < y + 3 ? cout << "Yes" << endl : cout << "No" << endl;
-	else  x + 3 > y ? cout << "Yes" << endl : cout << "No" << endl;
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	vector<int> b(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a.at(i);
+	}
+	for (int i = 0; i < n; i++) {
+		cin >> b.at(i);
+	}
+
+	int res = 0;
+	for (int i = 0; i < n; i++) {
+		res += a.at(i) * b.at(i);
+	}
+	res == 0 ? cout << "Yes" << endl : cout << "No" << endl;
 	return 0;
 }
