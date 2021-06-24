779eb2774d7a5e2e899ef54784af2a7c002a3e68
monjara <yaginumee@gmail.com>
2021-06-24 17:07:04 +0900

    171-B

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -3,8 +3,17 @@
 using namespace std;
 
 int main() {
-	char ch;
-	cin >> ch;
-	islower(ch) ? cout << 'a' << endl : cout << "A" << endl;
+	int n, k;
+	cin >> n >> k;
+	vector<int> p(n);
+	for (int i = 0; i < n; i++) {
+		cin >> p.at(i);
+	}
+	sort(p.begin(), p.end());
+	int res = 0;
+	for (int i = 0; i < k; i++) {
+		res += p.at(i);
+	}
+	cout << res << endl;
 	return 0;
 }
