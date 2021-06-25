aae8dcff0e22e096c764b5bd8858f3f0f828b1a4
monjara <yaginumee@gmail.com>
2021-06-25 23:29:09 +0900

    176-A

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -3,14 +3,9 @@
 using namespace std;
 
 int main() {
-	string s;
-	cin >> s;
-	int res;
-	if (s[0] == 'R' && s[1] == 'R' && s[2] == 'R') res = 3;
-	else if (s[0] == 'R' && s[1] == 'R') res = 2;
-	else if (s[1] == 'R' && s[2] == 'R') res = 2;
-	else if (s[0] == 'R' || s[1] == 'R' || s[2] == 'R') res = 1;
-	else res = 0;
+	int n, x, t;
+	cin >> n >> x >> t;
+	int res = n % x == 0 ? n / x * t : (n / x + 1) * t;
 	cout << res << endl;
 	return 0;
 }
