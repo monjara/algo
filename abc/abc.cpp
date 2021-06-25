8b4e56505fee3abd44779e78377e8f510838305c
monjara <yaginumee@gmail.com>
2021-06-25 18:34:25 +0900

    206-B

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -5,9 +5,13 @@ using namespace std;
 int main() {
 	int n;
 	cin >> n;
-	int res = n * 1.08;
-	if (res < 206) cout << "Yay!" << endl;
-	else if (res == 206) cout << "so-so" << endl;
-	else cout << ":(" << endl;
+	int money = 0;
+	int i = 1; 
+	while (1) {
+		money += i;
+		if (money >= n) break;
+		i++;
+	}
+	cout << i << endl;
 	return 0;
 }
