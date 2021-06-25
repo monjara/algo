7eb09aed3efd625f61df41ac3ed338af4861b379
monjara <yaginumee@gmail.com>
2021-06-25 23:50:31 +0900

    176-C

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -3,10 +3,21 @@
 using namespace std;
 
 int main() {
-	string n;
+	int n;
 	cin >> n;
-	int sum = 0;
-	for (int i = 0; i < n.size(); i++) sum += n[i] - '0';
-	sum % 9 == 0 ? cout << "Yes" << endl : cout << "No" << endl;
+	long long count = 0;
+	int tmp = 0;
+	for (int i = 0; i < n; i++) {
+		int a;
+		cin >> a;
+
+		if (i == 0) tmp = a;
+		while (tmp > a) {
+			a++;
+			count++;
+		}
+		tmp = a;
+	}
+	cout << count << endl;
 	return 0;
 }
