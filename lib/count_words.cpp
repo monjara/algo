9a33d24514a989c6f0683d871dd59f66b4ac6a7c
monjara <yaginumee@gmail.com>
2021-06-25 19:13:48 +0900

    library作成

---
+++ lib/count_words.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int find(string s, const string& word) {
+  int count = 0;
+  for (string::size_type pos = 0; (pos = s.find(word, pos)) != string::npos; pos += word.size()) {
+    ++count;
+  }
+  return count;
+}
+
+int main() {
+	string s;
+	cin >> s;
+	int count = find(s, "ZONe");
+	cout << count << endl;
+	return 0;
+}
