8f78d9c2b88ab24d341c40ffd4ddddce380bdfe5
monjara <yaginumee@gmail.com>
2021-06-25 20:58:03 +0900

    174-A

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -3,15 +3,8 @@
 using namespace std;
 
 int main() {
-	int n;
-	double D, H;
-	cin >> n >> D >> H;
-	double ans = 0;
-	for (int i = 0; i < n; i++) {
-		double d, h;
-		cin >> d >> h;
-		ans = max(ans, h - d * (H - h) / (D - d));
-	}
-	cout << fixed << setprecision(15) << ans << endl;
+	int x;
+	cin >> x;
+	x >= 30 ? cout << "Yes" << endl : cout << "No" << endl;
 	return 0;
 }
