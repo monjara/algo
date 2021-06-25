d12c3b5c2d42a94499468f66770c053b9d2ff28b
monjara <yaginumee@gmail.com>
2021-06-25 19:37:46 +0900

    hello-space-B

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -2,18 +2,16 @@
 
 using namespace std;
 
-int find(string s, const string& word) {
-  int count = 0;
-  for (string::size_type pos = 0; (pos = s.find(word, pos)) != string::npos; pos += word.size()) {
-    ++count;
-  }
-  return count;
-}
-
 int main() {
-	string s;
-	cin >> s;
-	int count = find(s, "ZONe");
-	cout << count << endl;
+	int n;
+	double D, H;
+	cin >> n >> D >> H;
+	double ans = 0;
+	for (int i = 0; i < n; i++) {
+		double d, h;
+		cin >> d >> h;
+		ans = max(ans, h - d * (H - h) / (D - d));
+	}
+	cout << fixed << setprecision(15) << ans << endl;
 	return 0;
 }
