62b90e8d6663d510c47489f6efee0c461dbb7a29
monjara <yaginumee@gmail.com>
2021-06-26 00:41:49 +0900

    177-A

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -3,21 +3,8 @@
 using namespace std;
 
 int main() {
-	int n;
-	cin >> n;
-	long long count = 0;
-	int tmp = 0;
-	for (int i = 0; i < n; i++) {
-		int a;
-		cin >> a;
-
-		if (i == 0) tmp = a;
-		while (tmp > a) {
-			a++;
-			count++;
-		}
-		tmp = a;
-	}
-	cout << count << endl;
+	double d, t, s;
+	cin >> d >> t >> s;
+	d / s <= t ? cout << "Yes" << endl : cout << "No" << endl;
 	return 0;
 }
