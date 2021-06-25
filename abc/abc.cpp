9b2e73e98c6d787dbeed5eb2ed9b0e2afa29127a
monjara <yaginumee@gmail.com>
2021-06-25 21:09:52 +0900

    174-B

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -3,8 +3,14 @@
 using namespace std;
 
 int main() {
-	int x;
-	cin >> x;
-	x >= 30 ? cout << "Yes" << endl : cout << "No" << endl;
+	int n, d;
+	cin >> n >> d;
+	int count = 0;
+	for (int i = 0; i < n; i++) {
+		int x, y;
+		cin >> x >> y;
+		if (pow(d, 2) >= pow(x, 2) + pow(y, 2)) count++;
+	}
+	cout << count << endl;
 	return 0;
 }
