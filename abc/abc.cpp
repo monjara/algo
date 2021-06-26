a7ae14aff0c2ce21aaaa06436c1693dc93a1af56
monjara <yaginumee@gmail.com>
2021-06-26 09:44:49 +0900

    178-A

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -3,23 +3,8 @@
 using namespace std;
 
 int main() {
-	int n;
-	cin >> n;
-	vector<int> a(n);
-	vector<long long> b(n + 1, 0);
-	int mod = 1000000007;
-
-	for (int i = 0; i < n; i++) {
-		cin >> a[i];
-		b[i + 1] = b[i] + a[i];
-	}
-
-	long long res = 0;
-	for (int i = 0; i < n; i++) {
-		long long sum = (b[n] - b[i+1]) % mod;
-		res += a[i] * sum;
-		res %= mod;
-	}
-	cout << res << endl;
+	int x;
+	cin >> x;
+	x == 0 ? cout << 1 << endl : cout << 0 << endl;
 	return 0;
 }
