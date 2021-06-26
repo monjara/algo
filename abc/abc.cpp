c20e01e829fad4b1311a44c9c53fbbc2c0c68773
monjara <yaginumee@gmail.com>
2021-06-26 23:42:14 +0900

    173-B

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -1,11 +1,21 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
-int main() {
-	long long a, b, c, d;
-	cin >> a >> b >> c >> d;
-	long long res = max(max(max(a * c, b * d), a * d), b * c);
-	cout << res << endl;
-	return 0;
+ 
+int main(){
+	int n;
+	cin >> n;
+	vector<int> ans(4, 0);
+	for (int i = 0; i < n; i++) {
+		string st;
+		cin >> st;
+		if (st[0] == 'A') ans.at(0)++;
+		else if (st[0] == 'W') ans.at(1)++;
+		else if (st[0] == 'T') ans.at(2)++;
+		else if (st[0] == 'R') ans.at(3)++;
+	}
+	cout << "AC x " << ans.at(0) << endl;
+	cout << "WA x " << ans.at(1) << endl;
+	cout << "TLE x " << ans.at(2) << endl;
+	cout << "RE x " << ans.at(3) << endl;
+    return 0;
 }
