999c094a274d08bbd60a3b8ca8d2a635b9b9ce4b
monjara <yaginumee@gmail.com>
2021-06-27 16:34:37 +0900

    154-B

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -1,17 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
  
 int main(){
-	ll h, n;
-	cin >> h >> n;
-	vector<int> a(n);
-	for (int i = 0; i < n; i++) {
-		int a;
-		cin >> a;
-		h -= a;
+	string s;
+	cin >> s;
+	for (int i = 0; i < s.size(); i++) {
+		cout << "x";
 	}
-	if (h > 0) cout << "No" << endl;
-	else cout << "Yes" << endl;
+	cout << endl;
     return 0;
 }
