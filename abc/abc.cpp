ea1ae3b3280d753cb1651d37c147d132d80ab6e3
monjara <yaginumee@gmail.com>
2021-06-27 15:15:28 +0900

    164-B

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -2,20 +2,19 @@
 using namespace std;
  
 int main(){
-	int n;
-	cin >> n;
-	vector<int> ans(4, 0);
-	for (int i = 0; i < n; i++) {
-		string st;
-		cin >> st;
-		if (st[0] == 'A') ans.at(0)++;
-		else if (st[0] == 'W') ans.at(1)++;
-		else if (st[0] == 'T') ans.at(2)++;
-		else if (st[0] == 'R') ans.at(3)++;
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	while (1) {
+		c -= b;
+		if (c <= 0) {
+			cout << "Yes" << endl;
+			return 0;
+		}
+		a -= d;
+		if (a <= 0) {
+			cout << "No" << endl;
+			return 0;
+		}
 	}
-	cout << "AC x " << ans.at(0) << endl;
-	cout << "WA x " << ans.at(1) << endl;
-	cout << "TLE x " << ans.at(2) << endl;
-	cout << "RE x " << ans.at(3) << endl;
     return 0;
 }
