229dff48216c6282358e7e75aaf77a48502c866b
monjara <yaginumee@gmail.com>
2021-06-27 22:56:53 +0900

    163-B

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -2,19 +2,14 @@
 using namespace std;
  
 int main(){
-	double n;
-	cin >> n;
-	double p = 0;
-	vector<double> x(n);
-	for (int i = 0; i < n; i++) {
-		cin >> x.at(i);
-		p += x.at(i);
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		int a;
+		cin >> a;
+		n -= a;
 	}
-	int ava = round(p / n);
-	int res = 0;
-	for (int i = 0; i < n; i++) {
-		res += pow(x.at(i) - ava, 2);
-	}
-	cout << res << endl;
+	if (n >= 0) cout << n << endl;
+	else cout << -1 << endl;
     return 0;
 }
