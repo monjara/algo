a8c9c8c23c929b59cc56f267cbc2d2ba2eec9f3f
monjara <yaginumee@gmail.com>
2021-06-28 15:19:20 +0900

    159-C

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -1,18 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 
 int main(){
-	string s1;
-	cin >> s1;
-	int n = s1.size();
-	string s2 = s1.substr(0, n / 2);
-	string s3 = s1.substr((n + 3) / 2 - 1);
-
-	bool ok = true;
-	if (s1 != string(s1.rbegin(), s1.rend())) ok = false;
-	if (s2 != string(s2.rbegin(), s2.rend())) ok = false;
-	if (s3 != string(s3.rbegin(), s3.rend())) ok = false;
-	ok ? cout << "Yes" << endl : cout << "No" << endl;
+	double l;
+	cin >> l;
+	double ans = pow(l, 3) / 27;
+	cout << fixed << setprecision(12) << ans << endl;
     return 0;
 }
