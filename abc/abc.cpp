6fe555b0f44d31a750a4fc9b9c53d29047b1bdf9
monjara <yaginumee@gmail.com>
2021-06-28 15:25:52 +0900

    PPC2020-A

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main(){
-	double l;
-	cin >> l;
-	double ans = pow(l, 3) / 27;
-	cout << fixed << setprecision(12) << ans << endl;
+	vector<int> a{1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51};
+	int k;
+	cin >> k;
+	cout << a.at(k - 1) << endl;
     return 0;
 }
