a801591bf1919cadb0a3a83c85745de8ca29b99b
monjara <yaginumee@gmail.com>
2021-06-28 17:57:30 +0900

    158-B

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -1,12 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 
 int main(){
-	ll n, a, b;
-	cin >> n >> a >> b;
-	ll br = a + b;
-	ll ans = (n / br) * a + min(a, n % br);
-	cout << ans << endl;
+	int a, b;
+	cin >> a >> b;
+
+	for (int i = 1; i <= 10000; i++) {
+		int za = i * 8 / 100;
+		int zb = i * 10 / 100;
+		if (za == a && zb == b) {
+			cout << i << endl;
+			return 0;
+		}
+	}
+	cout << -1 << endl;
     return 0;
 }
