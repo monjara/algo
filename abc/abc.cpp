c3d5eca068a3f72947c8448c7f9bf49ab85d58ac
monjara <yaginumee@gmail.com>
2021-06-28 15:13:37 +0900

    159-B

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -3,12 +3,16 @@ using namespace std;
 using ll = long long;
 
 int main(){
-	int n;
-	cin >> n;
-	int res = 0;
-	res += 1000 * (n / 500);
-	n %= 500;
-	res += 5 * (n / 5);
-	cout << res << endl;
+	string s1;
+	cin >> s1;
+	int n = s1.size();
+	string s2 = s1.substr(0, n / 2);
+	string s3 = s1.substr((n + 3) / 2 - 1);
+
+	bool ok = true;
+	if (s1 != string(s1.rbegin(), s1.rend())) ok = false;
+	if (s2 != string(s2.rbegin(), s2.rend())) ok = false;
+	if (s3 != string(s3.rbegin(), s3.rend())) ok = false;
+	ok ? cout << "Yes" << endl : cout << "No" << endl;
     return 0;
 }
