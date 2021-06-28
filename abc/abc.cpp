eb451bd6cfde251b7ac8e23934c263e5ada8e1a1
monjara <yaginumee@gmail.com>
2021-06-28 15:47:28 +0900

    PPC2020-B

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -1,10 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
 int main(){
-	vector<int> a{1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51};
-	int k;
-	cin >> k;
-	cout << a.at(k - 1) << endl;
+	ll h, w;
+	cin >> h >> w;
+	ll ans;
+	if (h == 1 || w == 1) ans = 1;
+	else if (h % 2 == 0) ans = ((h / 2) * w);
+	else if (w % 2 == 0) ans = (h * w) / 2;
+	else ans = ((h * w) / 2) + 1; 
+	cout << ans << endl;
     return 0;
 }
