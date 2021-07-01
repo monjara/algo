8b5645d803e063e9007ca16ffa462a04ab6d5d85
monjara <yaginumee@gmail.com>
2021-07-01 23:50:57 +0900

    141-C

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -4,18 +4,21 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-	string s;
-	cin >> s;
-	bool ok = true;
-	rep(i, s.size()) {
-		if (i % 2 == 0) {
-			if (s[i] == 'L') ok = false;
-		}
-		else {
-			if (s[i] == 'R') ok = false;
-		}
+	int n, k, q;
+	cin >> n >> k >> q;
+	map<int, int> cnt;
+	rep(i, n) {
+		cnt[i + 1] = k;
 	}
-	if (ok) cout << "Yes" << endl;
-	else cout << "No" << endl;
+	rep(i, q) {
+		int a;
+		cin >> a;
+		cnt[a]++;
+	}
+	rep(i, n) {
+		cnt[i + 1] -= q;
+		if (cnt[i + 1] > 0) cout << "Yes" << endl;
+		else cout << "No" << endl;
+	}
     return 0;
 }
