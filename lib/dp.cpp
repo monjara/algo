04c8f54ca7bf00c56f71d748d1ce0ca4ec1cf7b1
monjara <yaginumee@gmail.com>
2021-07-02 19:16:27 +0900

    dp

---
+++ lib/dp.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const long long INF = 1LL << 60;
+
+int main() {
+	// https://atcoder.jp/contests/dp/tasks/dp_a
+	int n, h[100010];
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> h[i];
+
+	long long dp[100010];
+	for (int i = 0; i < 100010; i++) dp[i] = INF;
+
+	dp[0] = 0;
+	for (int i = 0; i < n; i++) {
+		chmin(dp[i + 1], dp[i] + abs(h[i] - h[i + 1]));
+		chmin(dp[i + 2], dp[i] + abs(h[i] - h[i + 2]));
+	}
+	cout << dp[n - 1] << endl;
+	return 0;
+}
