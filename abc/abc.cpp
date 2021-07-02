e492e80582e76c942ee6a1a5791ad544e2218126
monjara <yaginumee@gmail.com>
2021-07-02 14:21:11 +0900

    147-B

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -4,17 +4,12 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-	int n;
-	cin >> n;
-	map<int, int> cnt;
-	for (int i = 1; i <= n; i++) {
-		int a;
-		cin >> a;
-		cnt[a] = i;
+	string s;
+	cin >> s;
+	int cnt = 0;
+	rep(i, s.size() / 2) {
+		if (s[i] != s[s.size() - i - 1]) cnt++;
 	}
-	for (int i = 1; i <= n; i++) {
-		if (i == n) cout << cnt[i] << endl;
-		else cout << cnt[i] << " ";
-	}
+	cout << cnt << endl;
     return 0;
 }
