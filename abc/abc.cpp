8f491c44d1d61285915346ca7aaa6e93d88f82ae
monjara <yaginumee@gmail.com>
2021-07-02 18:43:26 +0900

    三井住友-B

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -4,9 +4,13 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    int m_1, d_1, m_2, d_2;
-    cin >> m_1 >> d_1 >> m_2 >> d_2;
-    if (d_2 == 1) cout << 1 << endl;
-    else cout << 0 << endl;
+    double n;
+    cin >> n;
+    double res = n / 108 * 100;
+    res = ceil(res);
+    bool ok = true;
+    if (floor(res * 1.08) != n) ok = false;
+    if (ok) cout << res << endl;
+    else cout << ":(" << endl;
     return 0;
 }
