ffdc72e148be56a8b9008add9d796d64210548a7
monjara <yaginumee@gmail.com>
2021-07-03 22:37:36 +0900

    139-B

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -1,27 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 typedef long long ll;
 
-template<class T> inline bool chmin(T& a, T b) {
-    if (a > b) {
-        a = b;
-        return true;
-    }
-    return false;
-}
-
 int main(){
-    ll n, k, h[101000], dp[101000];
-	ll INF = 1LL << 60;
-    cin >> n >> k;
-    for (int i = 0; i < n; i++) cin >> h[i];
-    for (int i = 0; i < 101000; i++) dp[i] = INF;
-    dp[0] = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 1; j <= k; j++) {
-            chmin(dp[i + j], dp[i] + abs(h[i] - h[i + j]));
-        }
-    }
-    cout << dp[n - 1] << endl;
+    int a, b;
+    cin >> a >> b;
+    int res = (b - 1 + a - 2) / (a - 1);
+    cout << res << endl;
     return 0;
 }
