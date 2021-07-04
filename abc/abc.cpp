2ebe60dec02833cc7463dc501553498e9e990294
monjara <yaginumee@gmail.com>
2021-07-04 23:28:53 +0900

    208-A

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -5,21 +5,9 @@ template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true
 typedef long long ll;
 
 int main(){
-    ll n, h[110000];
-    cin >> n;
-    for (int i = 0; i < n; i++) cin >> h[i];
-
-    bool checked[110000];
-    int ans = 0;
-    for (int i = 0; i < n; i++) {
-        if (!checked[i]) {
-            for (int j = i + 1; j < n; j++) { 
-                if (h[j - 1] < h[j]) break;
-                checked[j] = true;
-                chmax(ans, j - i);
-            } 
-        }
-    }
-    cout << ans << endl;
+    int a, b;
+    cin >> a >> b;
+    if (a * 6 >= b && a * 1 <= b) cout << "Yes" << endl;
+    else cout << "No" << endl;
     return 0;
 }
