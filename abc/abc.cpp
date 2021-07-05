dec0e7b8b55a925dd3d57c871ed6aa545e316ddb
monjara <yaginumee@gmail.com>
2021-07-05 16:08:54 +0900

    198-A

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -5,29 +5,9 @@ template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true
 typedef long long ll;
 
 int main(){
-    ll n, k;
-    cin >> n >> k;
-    map<ll, ll> cnt;
-    vector<ll> peopleNum(n);
-    vector<ll> mapOrder(n);
-    for (ll i = 0; i < n; i++) {
-        ll num;
-        cin >> num;
-        cnt[num] = k / n;
-        peopleNum.at(i) = num;
-        mapOrder.at(i) = num;
-    }
-    k %= n;
-    sort(peopleNum.begin(), peopleNum.end());
-    while (k > 0) {
-        for (ll i = 0; i < n; i++) {
-            if (k < 1) break;
-            cnt[peopleNum.at(i)]++;
-            k--;
-        }
-    }
-    for (int i = 0; i < n; i++) {
-        cout << cnt[mapOrder.at(i)] << endl;
-    }
+    int n;
+    cin >> n;
+    int res = n - 1;
+    cout << res << endl;
     return 0;
 }
