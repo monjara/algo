64c9e2ccd119509c31ae82a530a97955fd3ac294
monjara <yaginumee@gmail.com>
2021-07-11 00:15:24 +0900

    ARC準備

---
+++ arc/arc.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+typedef long long ll;
+
+int main(){
+    return 0;
+}
