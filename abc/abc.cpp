3299bf512bcab38fea843faab1c7753a7aea8850
monjara <yaginumee@gmail.com>
2021-07-11 20:45:35 +0900

    96-B

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -5,13 +5,15 @@ template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true
 typedef long long ll;
 
 int main(){
-    int a, b;
-    cin >> a >> b;
-    if (a <= b) {
-        cout << a << endl;
+    vector<int> abc(3);
+    for (int i = 0; i < 3; i++) cin >> abc.at(i);
+    int k;
+    cin >> k;
+    sort(abc.begin(), abc.end());
+    for (int i = 0; i < k; i++) {
+        abc.at(2) *= 2;
     }
-    else {
-        cout << a - 1 << endl;
-    }
+    int ans = accumulate(abc.begin(), abc.end(), 0);
+    cout << ans << endl;
     return 0;
 }
