6d94dcf4122bc29a69821ec4218b08b94f83ff1b
monjara <yaginumee@gmail.com>
2021-07-12 16:44:56 +0900

    140-C

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -4,25 +4,20 @@ template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 typedef long long ll;
 
-int main(){
+int main() {
     int n;
     cin >> n;
-    vector<ll> a(n + 1);
-    vector<ll> b(n);
-    for (int i = 0; i < n + 1; i++) cin >> a.at(i);
-    for (int i = 0; i < n; i++) cin >> b.at(i);
-    ll cnt = 0;
+    vector<int> a(n);
+    vector<int> b(n - 1);
+    for (int i = 0; i < n - 1; i++) cin >> b.at(i);
     for (int i = 0; i < n; i++) {
-        ll f = min(a.at(i), b.at(i));
-        a.at(i) -= f;
-        b.at(i) -= f;
-        cnt += f;
-
-        ll s = min(a.at(i) + a.at(i + 1), b.at(i));
-        a.at(i + 1) -= s;
-        b.at(i) -= s;
-        cnt += s;
+        if (i == 0) a.at(i) = b.at(i);
+        else if (i == n - 1) a.at(i) = b.at(i - 1);
+        else {
+            a.at(i) = min(b.at(i - 1), b.at(i));
+        }
     }
-    cout << cnt << endl;
+    ll ans = accumulate(a.begin(), a.end(), 0);
+    cout << ans << endl;
     return 0;
 }
