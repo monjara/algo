3f5bdf2e328263ba9533cc4f966fb205b242f0e3
monjara <yaginumee@gmail.com>
2021-07-12 22:54:45 +0900

    CADDi-A

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -5,16 +5,12 @@ template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true
 typedef long long ll;
 
 int main() {
-    int n, k;
-    cin >> n >> k;
-    vector<ll> h(n);
-    for (int i = 0; i < n; i++) cin >> h.at(i);
-    sort(h.begin(), h.end());
-    ll ans = 1100000000;
-    for (int i = 0; i < n - k + 1; i++) {
-        ll tmp = i + k - 1;
-        ans = min(ans, h.at(tmp) - h.at(i));
+    string n;
+    cin >> n;
+    int cnt = 0;
+    for (int i = 0; i < 4; i++) {
+        if (n[i] == '2') cnt++;
     }
-    cout << ans << endl;
+    cout << cnt << endl;
     return 0;
 }
