3889fd3193eae56d9fb8518f787b750a024bff9b
monjara <yaginumee@gmail.com>
2021-07-15 23:52:20 +0900

    168-B

--- arc/arc.cpp
+++ arc/arc.cpp
@@ -5,15 +5,16 @@ template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true
 typedef long long ll;
 
 int main() {
-	int n;
-	cin >> n;
-	if (n % 10 == 3) cout << "bon" << endl;
-	else if (n % 10 == 0 ||
-	         n % 10 == 1 ||
-			 n % 10 == 6 ||
-			 n % 10 == 8) {
-				 cout << "pon" << endl;
-			 }
-	else cout << "hon" << endl;
+	int k;
+	string s;
+	cin >> k >> s;
+	if (s.size() <= k) cout << s;
+	else {
+		for (int i = 0; i < k; i++) {
+			cout << s[i];
+		}
+		cout << "...";
+	}
+	cout << endl;
     return 0;
 }
