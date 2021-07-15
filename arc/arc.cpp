31eb567cbc0e4236849171e498701b238927289c
monjara <yaginumee@gmail.com>
2021-07-15 23:35:41 +0900

    148-A

--- arc/arc.cpp
+++ arc/arc.cpp
@@ -4,15 +4,9 @@ template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 typedef long long ll;
 
-int main(){
-	vector<ll> abc(3);
-	for (int i = 0; i < 3; i++) cin >> abc.at(i);
-	ll mod = 998244353;
-	ll ans = 1;
-	for (int i = 0; i < 3; i++) {
-		ans *= (abc.at(i) * (abc.at(i) + 1)) / 2;
-		ans %= mod;
-	}
-	cout << ans << endl;
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << 6 - (a + b) << endl;
     return 0;
 }
