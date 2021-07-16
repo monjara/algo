d49725d36bddcc6a33b300c63fd8249599730fd2
monjara <yaginumee@gmail.com>
2021-07-16 15:14:06 +0900

    116-B 別解

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -7,18 +7,15 @@ typedef long long ll;
 int main() {
     int s;
     cin >> s;
-    map<int, int> a;
-    a[s] = 1;
+    set<int> a;
+    a.insert(s);
     int tmp = s;
     for (int i = 2; i < 1100000; i++) {
-        if (tmp % 2 == 0) {
-            tmp = tmp / 2;
-        }
-        else {
-            tmp = 3 * tmp + 1;
-        }
-        a[tmp]++;
-        if (a[tmp] == 2) {
+        if (tmp % 2 == 0) tmp = tmp / 2;
+        else tmp = 3 * tmp + 1;
+
+        auto res = a.insert(tmp);
+        if (!res.second) {
             cout << i << endl;
             break;
         }
