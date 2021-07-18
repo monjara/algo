d695178d19f21519dc6a56fdca769e09fc5c0344
monjara <yaginumee@gmail.com>
2021-07-18 23:59:41 +0900

    210-A

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -3,11 +3,15 @@ using namespace std;
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 typedef long long ll;
-
+ 
 int main() {
-    int a, p;
-    cin >> a >> p;
-    int ans = (a * 3 + p) / 2;
+    int n, a, x, y;
+    cin >> n >> a >> x >> y;
+    int ans;
+    if (n >= a) {
+        ans = a * x + (n - a) * y;
+    }
+    else ans = n * x;
     cout << ans << endl;
     return 0;
 }
