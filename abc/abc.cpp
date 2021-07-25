26b8a4f744b4a5c81f60967d35d38cf9c3ebba72
monjara <yaginumee@gmail.com>
2021-07-25 21:04:53 +0900

    211-A

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -4,26 +4,10 @@ template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 typedef long long ll;
 
-bool isPrime(int num) {
-    if (num < 2) return false;
-    else if (num == 2) return true;
-    else if (num % 2 == 0) return false;
-    else {
-        double sqrtNum = sqrt(num);
-        for (int i = 3; i <= sqrtNum; i += 2) {
-            if (num % i == 0) return false;
-        }
-        return true;
-    }
-}
-
 int main() {
-    int x;
-    cin >> x;
-    while (true) {
-        if (isPrime(x)) break;
-        x++;
-    }
-    cout << x << endl;
+    double a, b;
+    cin >> a >> b;
+    double c = (a - b) / 3 + b;
+    cout << fixed << setprecision(15) << c << endl;
     return 0;
 }
