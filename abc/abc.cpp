5f1eca78bf29344a7a1d6eb89ed9ad7548d96ebe
monjara <yaginumee@gmail.com>
2021-07-28 00:28:30 +0900

    ACL-A

--- abc/abc.cpp
+++ abc/abc.cpp
@@ -5,27 +5,9 @@ template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true
 typedef long long ll;
 
 int main() {
-    int n, q;
-    string s;
-    cin >> n >> q >> s;
-
-    vector<int> ac_cnt(n - 1, 0);
-    for (int i = 0; i < n - 1; i++) {
-        if (s[i] == 'A' && s[i + 1] == 'C') ac_cnt.at(i) = 1;
-    }
-
-    vector<int> sm(n, 0);
-    for (int i = 0; i < n - 1; i++) {
-        sm.at(i + 1) = sm.at(i) + ac_cnt.at(i);
-    }
-
-    for (int i = 0; i < q; i++) {
-        int l, r;
-        cin >> l >> r;
-        l--;
-        r--;
-        int cnt = sm.at(r) - sm.at(l);
-        cout << cnt << endl;
-    }
+    int k;
+    cin >> k;
+    for (int i = 0; i < k; i++) cout << "ACL";
+    cout << endl;
     return 0;
 }
