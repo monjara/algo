db5d20fb0b816d98a950f3781ea0dac8008c7826
monjara <yaginumee@gmail.com>
2021-10-13 22:21:08 +0900

    pra

--- abc/cpp/abc.cpp
+++ abc/cpp/abc.cpp
@@ -4,16 +4,33 @@ template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 typedef long long ll;
 
-int main() {
-	int n, s, d;
-	cin >> n >> s >> d;
-	bool ok = false;
+void output(vector<int> a, int n) {
 	for (int i = 0; i < n; i++) {
-		int x, y;
-		cin >> x >> y;
-		if (x < s && y > d) ok = true;
+		if (i > 0) cout << " ";
+		cout << a.at(i);
 	}
-	if (ok) cout << "Yes" << endl;
-	else cout << "No" << endl;
+	cout << endl;
+}
+
+void insectionSort(vector<int> a, int n) {
+	for (int i = 0; i < n; i++) {
+		int v = a.at(i);
+		int j = i - 1;
+		while (j >= 0 && a.at(j) > v) {
+			a.at(j + 1) = a.at(j);
+			j--;
+		}
+		a.at(j + 1) = v;
+		output(a, n);
+	}
+}
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) cin >> a.at(i);
+
+	insectionSort(a, n);
 	return 0;
 }
