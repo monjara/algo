8bb466ce38e35fc0b13da8de864223a15f0d9da6
monjara <yaginumee@gmail.com>
2021-10-14 21:29:18 +0900

    pra

--- abc/cpp/abc.cpp
+++ abc/cpp/abc.cpp
@@ -4,18 +4,17 @@ template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 typedef long long ll;
 
-int bubbleSort(int a[], int n) {
+int sectionSort(int a[], int n) {
 	int cnt = 0;
-	bool flag = true;
-	while (flag) {
-		flag = false;
-		for (int i = n - 1; i > 0; i--) {
-			if (a[i] < a[i - 1]) {
-				swap(a[i], a[i - 1]);
-				flag = true;
-				cnt++;
+	for (int i = 0; i < n; i++) {
+		int minj = i;
+		for (int j = i; j < n; j++) {
+			if (a[j] < a[minj]) {
+				minj = j;
 			}
 		}
+		if (a[i] != a[minj]) cnt++;
+		swap(a[i], a[minj]);
 	}
 	return cnt;
 }
@@ -25,10 +24,11 @@ int main() {
 	cin >> n;
 	int a[n];
 	for (int i = 0; i < n; i++) cin >> a[i];
-	int cnt = bubbleSort(a, n);
+	int cnt = sectionSort(a, n);
 	for (int i = 0; i < n; i++) {
 		if (i > 0) cout << " ";
 		cout << a[i];
 	}
 	cout << endl << cnt << endl;
+	return 0;
 }
