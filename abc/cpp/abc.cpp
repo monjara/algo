5e9f0ff115612168527a1df92824991bfeb7c6ce
monjara <yaginumee@gmail.com>
2021-10-15 20:26:25 +0900

    222-A

--- abc/cpp/abc.cpp
+++ abc/cpp/abc.cpp
@@ -4,31 +4,11 @@ template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 typedef long long ll;
 
-int sectionSort(int a[], int n) {
-	int cnt = 0;
-	for (int i = 0; i < n; i++) {
-		int minj = i;
-		for (int j = i; j < n; j++) {
-			if (a[j] < a[minj]) {
-				minj = j;
-			}
-		}
-		if (a[i] != a[minj]) cnt++;
-		swap(a[i], a[minj]);
-	}
-	return cnt;
-}
-
 int main() {
-	int n;
+	string n;
 	cin >> n;
-	int a[n];
-	for (int i = 0; i < n; i++) cin >> a[i];
-	int cnt = sectionSort(a, n);
-	for (int i = 0; i < n; i++) {
-		if (i > 0) cout << " ";
-		cout << a[i];
-	}
-	cout << endl << cnt << endl;
+	int zeroLength = 4 - n.length();
+	for (int i = 0; i < zeroLength; i++) cout << "0";
+	cout << n << endl;
 	return 0;
 }
