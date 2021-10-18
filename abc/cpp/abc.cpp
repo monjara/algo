454f37dfbe7f47819e98c3c4e46c70a69fe6d6a3
monjara <yaginumee@gmail.com>
2021-10-18 22:02:36 +0900

    pra

--- abc/cpp/abc.cpp
+++ abc/cpp/abc.cpp
@@ -4,10 +4,21 @@ template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 typedef long long ll;
 
+void randomAccess(vector<ll> a, int p) {
+	cout << a.at(p) << endl;
+}
+
 int main() {
-	int n;
-	cin >> n;
-	if (n % 2 == 0) cout << "White" << endl;
-	else cout << "Black" << endl;
+	int q;
+	cin >> q;
+	vector<int> a;
+	for (int i = 0; i < q; i++) {
+		int idx, x;
+		cin >> idx;
+		if (idx != 2) cin >> x;
+		if (idx == 0) a.push_back(x);
+		else if (idx == 1) cout << a.at(x) << endl;;
+		else if (idx == 2) a.pop_back();
+	}
 	return 0;
 }
