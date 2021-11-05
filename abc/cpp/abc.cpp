21d90c7aa4dd11a698f8514b907946f33965f50f
monjara <yaginumee@gmail.com>
2021-11-06 00:13:51 +0900

    45-B

--- abc/cpp/abc.cpp
+++ abc/cpp/abc.cpp
@@ -4,14 +4,35 @@ template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 typedef long long ll;
 
+vector<pair<char, int>> runLengthEncoding(string s) {
+    int n = s.length();
+ 
+    vector<pair<char, int>> res;
+    char pre = s[0];
+    int cnt = 1;
+	for (int i = 1; i < n; i++) {
+        if (pre != s[i]) {
+            res.push_back({ pre, cnt });
+            pre = s[i];
+            cnt = 1;
+        }
+        else cnt++;
+    }
+ 
+    res.push_back({ pre, cnt });
+    return res;
+}
+
 int main() {
-	int n, k, x, y;
-	cin >> n >> k >> x >> y;
-	int ans = 0;
-	for (int i = 1; i <= n; i++) {
-		if (i <= k) ans += x;
-		else ans += y;
+	string w;
+	cin >> w;
+	sort(w.begin(), w.end());
+	auto rl = runLengthEncoding(w);
+	bool flg = true;
+	for (int i = 0; i < rl.size(); i++) {
+		if (rl.at(i).second % 2 == 1) flg = false;
 	}
-	cout << ans << endl;
+	if (flg) cout << "Yes" << endl;
+	else cout << "No" << endl;
 	return 0;
 }
