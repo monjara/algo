d662afe78dd8fbae4c929f3b803a09b42a89dd21
monjara <yaginumee@gmail.com>
2021-12-08 20:59:29 +0900

    pra

--- abc/cpp/abc.cpp
+++ abc/cpp/abc.cpp
@@ -1,28 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
-template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
-typedef long long ll;
 
 int main() {
-	int n;
-	cin >> n;
-	int zeroLength = 2;
-	int addAns = 0;
+	string s;
+	cin >> s;
 
-	if (10 <= n && n < 42) {
-		zeroLength = 1;
-	} else if (42 <= n && n < 100) {
-		zeroLength = 1;
-		addAns = 1;
-	} else if (100 <= n) {
-		zeroLength = 0;
-		addAns = 1;
+	regex re(R"(\d{3})");
+	bool check = regex_match(s, re);
+
+	if (!check) {
+		cout << "error" << endl;
+		return 0;
 	}
-
-	cout << "AGC";
-	for (int i = 0; i < zeroLength; ++i) cout << "0";
-	cout << n + addAns << endl;
-
+	cout << stoi(s) * 2 << endl;
 	return 0;
 }
