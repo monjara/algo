9e2fd43df82eb039b8c2592352b29c549751889a
monjara <yaginumee@gmail.com>
2021-12-08 22:56:07 +0900

    pra

--- abc/cpp/abc.cpp
+++ abc/cpp/abc.cpp
@@ -2,29 +2,36 @@
 using namespace std;
 
 int main() {
-	int n;
-	cin >> n;
-	vector<int> v(n, 0);
-	int x = 0;
-	int y = 0;
-	bool correct = true;
-	for (int i = 0; i < n; i++) {
-		int a;
-		cin >> a;
-		v.at(a - 1)++;
-		if (v.at(a-1) > 1) {
-			y = a;
-			correct = false;
+	string s;
+	cin >> s;
+	vector<string> v;
+
+	string tmp;
+	int check = 0;
+	for (int i = 0; i < s.length(); i++) {
+		tmp += s[i];
+		if (isupper(s[i])) {
+			check++;
+			if (check == 2) {
+				v.push_back(tmp);
+				tmp.clear();
+				check = 0;
+			}
 		}
 	}
-	if (correct) {
-		cout << "Correct" << endl;
-		return 0;
+	sort(v.begin(), v.end(), [](string& a, string& b) {
+		for (int i = 0; i < min(a.size(), b.size()); i++) {
+			char a_char = tolower(a[i]);
+			char b_char = tolower(b[i]);
+			if (a_char != b_char) {
+				return a_char < b_char;
+			}
+		}
+		return a.size() < b.size();
+	});
+	for (int i = 0; i < v.size(); i++) {
+		cout << v.at(i);
 	}
-
-	for (int i = 0; i < n; i++) {
-		if (v.at(i) == 0) x = i+1;
-	}
-	cout << y << " " << x << endl;
+	cout << endl;
 	return 0;
 }
