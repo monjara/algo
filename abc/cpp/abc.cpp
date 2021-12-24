4b69932a2696ead2600cc8e14300c352dccbaa38
monjara <yaginumee@gmail.com>
2021-12-24 23:38:41 +0900

    232-B

--- abc/cpp/abc.cpp
+++ abc/cpp/abc.cpp
@@ -2,9 +2,21 @@
 using namespace std;
 
 int main() {
-	char a, x, b;
-	cin >> a >> x >> b;
-	int ans = (int)(a - '0') * (int)(b - '0');
-	cout << ans << endl;
+	string s, t;
+	cin >> s >> t;
+	int length = s.length();
+	int tmp = t[0] - s[0];
+	if (tmp < 0) tmp = 26 + tmp;
+
+	for (int i = 1; i < length; i++) {
+		int n = t[i] - s[i];
+		if (n < 0) n = 26 + n;
+
+		if (tmp != n) {
+			cout << "No" << endl;
+			return 0;
+		}
+	}
+	cout << "Yes" << endl;
 	return 0;
 }
