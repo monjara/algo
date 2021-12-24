b18a78c508a4441bee14fee0059807a0b79811c0
monjara <yaginumee@gmail.com>
2021-12-24 23:19:44 +0900

    232-A

--- abc/cpp/abc.cpp
+++ abc/cpp/abc.cpp
@@ -2,14 +2,9 @@
 using namespace std;
 
 int main() {
-	int a, b, c;
-	cin >> a >> b >> c;
-	for (int i = a; i <= b; i++) {
-		if (i % c == 0) {
-			cout << i << endl;
-			return 0;
-		}
-	}
-	cout << "-1" << endl;
+	char a, x, b;
+	cin >> a >> x >> b;
+	int ans = (int)(a - '0') * (int)(b - '0');
+	cout << ans << endl;
 	return 0;
 }
