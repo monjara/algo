fc0dedf5e8659ecc86cd15ee6370ec8ed7d992fb
monjara <yaginumee@gmail.com>
2022-01-02 13:00:51 +0900

    231-A

--- abc/cpp/abc.cpp
+++ abc/cpp/abc.cpp
@@ -3,9 +3,23 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-	double d;
-	cin >> d;
-	double ans = d / 100;
+	int n;
+	cin >> n;
+	vector<string> vec(n);
+	for (int i = 0; i < n; i++) cin >> vec.at(i);
+	int tmp = -1;
+	string ans;
+	for (int i = 0; i < n; i++) {
+		string target = vec.at(i);
+		int cnt = 0;
+		for (int j = 0; j < n; j++) {
+			if (target == vec.at(j)) cnt++;
+		}
+		if (cnt > tmp) {
+			tmp = cnt;
+			ans = vec.at(i);
+		}
+	}
 	cout << ans << endl;
 	return 0;
 }
