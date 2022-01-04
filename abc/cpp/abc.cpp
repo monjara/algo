1043d0a30ba9845b737cac9c606cad0d19806c30
monjara <yaginumee@gmail.com>
2022-01-04 22:39:55 +0900

    231-B

--- abc/cpp/abc.cpp
+++ abc/cpp/abc.cpp
@@ -3,16 +3,17 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-	int n, q; 
-	cin >> n >> q;
-	vector<ll> a(n);
-	for (int i = 0; i < n; i++) cin >> a.at(i);
-	sort(a.begin(), a.end());
-	for (int i = 0; i < q; i++) {
-		int x;
-		cin >> x;
-		auto ans = a.end() - lower_bound(a.begin(), a.end(), x);
-		cout << ans << endl;
+	string a, b;
+	cin >> a >> b;
+	int size = min(a.length(), b.length());
+	reverse(a.begin(), a.end());
+	reverse(b.begin(), b.end());
+	for (int i = 0; i < size; i++) {
+		if ((a[i] - '0') + (b[i] - '0') > 9) {
+			cout << "Hard" << endl;
+			return 0;
+		}
 	}
+	cout << "Easy" << endl;
 	return 0;
 }
