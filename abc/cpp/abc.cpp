5468e8227f2c3e62bf67b2e26d7aed939e29655c
monjara <yaginumee@gmail.com>
2022-01-07 00:02:19 +0900

    42-C

--- abc/cpp/abc.cpp
+++ abc/cpp/abc.cpp
@@ -3,14 +3,29 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-	int n, l;
-	cin >> n >> l;
-	vector<string> v(n);
-	for (int i = 0; i < n; i++) cin >> v.at(i);
-	sort(v.begin(), v.end());
-	for (int i = 0; i < n; i++) {
-		cout << v.at(i);
+	int n, k;
+	cin >> n >> k;
+	vector<char> v(k);
+	for (int i = 0; i < k; i++) cin >> v.at(i);
+	while (true) {
+		string s_n = to_string(n);
+		bool check = true;
+		for (int i = 0; i < s_n.length(); i++) {
+			for (int j = 0; j < k; j++) {
+				if (s_n[i] == v.at(j)) {
+					check = false;
+				    break;
+				}
+			}
+			if (!check) break;
+		}
+
+		if (check) {
+			cout << n << endl;
+			break;
+		}
+
+		n++;
 	}
-	cout << endl;
 	return 0;
 }
