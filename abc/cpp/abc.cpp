090a716aca36f52e3a59e4e2d9e08e67ff6666f8
monjara <yaginumee@gmail.com>
2022-01-11 21:11:41 +0900

    pra

--- abc/cpp/abc.cpp
+++ abc/cpp/abc.cpp
@@ -3,15 +3,9 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-	int s, t, x;
-	cin >> s >> t >> x;
-    if (s <= t){
-        if (s <= x && x < t) cout << "Yes" << endl;
-        else cout << "No" << endl;
-    } 
-	else {
-        if (s <= x || x < t) cout << "Yes" << endl;
-        else cout << "No" << endl;
-    }
+	int n;
+	cin >> n;
+	int ans = (1 + n) * n / 2;
+	cout << ans << endl;
 	return 0;
 }
