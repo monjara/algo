f821b42dbc38b36b40ff7a5ba7ad8ab3defc0376
monjara <yaginumee@gmail.com>
2022-01-27 21:43:13 +0900

    236-A

--- abc/cpp/abc.cpp
+++ abc/cpp/abc.cpp
@@ -3,17 +3,15 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-	int n;
-	cin >> n;
-	int ans = -1;
-	for (int i = 0; i < n; i++) {
-		int tmp;
-		cin >> tmp;
-		if (ans >= tmp) {
-			cout << ans << endl;
-			return 0;
-		}
-		ans = tmp;
+	string s, ans;
+	int a, b;
+	cin >> s >> a >> b;
+	a--;
+	b--;
+	for (int i = 0; i < s.length(); i++) {
+		if (i == a) ans += s[b];
+		else if (i == b) ans += s[a];
+		else ans += s[i];
 	}
 	cout << ans << endl;
 	return 0;
