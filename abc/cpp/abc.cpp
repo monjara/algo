d9605e6410e76c7118e74174e4700ec6fe05adfd
monjara <yaginumee@gmail.com>
2022-01-30 22:56:31 +0900

    237-C

--- abc/cpp/abc.cpp
+++ abc/cpp/abc.cpp
@@ -3,20 +3,26 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-	int h, w;
-	cin >> h >> w;
-	vector<vector<int>> a(h, vector<int>(w));
-	for (int i = 0; i < h; i++) {
-		for (int j = 0; j < w; j++) {
-			cin >> a.at(i).at(j);
+	string s;
+	cin >> s;
+	int length = s.length();
+	int a_cnt = 0;
+	for (int i = length - 1; i >= 0; i--) {
+		if (s[i] == 'a') {
+			s.erase(i, 1);
+			a_cnt++;
 		}
+		else break;
 	}
-	for (int i = 0; i < w; i++) {
-		for (int j = 0; j < h; j++) {
-			cout << a.at(j).at(i);
-			if (j != h - 1) cout << " ";
-		}
-		cout << endl;
+	int a_cnt_2 = 0;
+	for (int i = 0; i < a_cnt; i++) {
+		if (s[i] == 'a') a_cnt_2++;
+		else break;
 	}
+	s.erase(0, a_cnt_2);
+	if (equal(s.begin(), s.begin() + s.size()/2, s.rbegin())) {
+		cout << "Yes" << endl;
+	}
+	else cout << "No" << endl;
 	return 0;
 }
