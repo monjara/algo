0cd9e30142361c95e09ed82596d85fbb8fd49007
monjara <yaginumee@gmail.com>
2022-02-03 21:25:54 +0900

    229-A

--- abc/cpp/abc.cpp
+++ abc/cpp/abc.cpp
@@ -3,26 +3,11 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-	string s;
-	cin >> s;
-	int length = s.length();
-	int a_cnt = 0;
-	for (int i = length - 1; i >= 0; i--) {
-		if (s[i] == 'a') {
-			s.erase(i, 1);
-			a_cnt++;
-		}
-		else break;
+	string s1, s2;
+	cin >> s1 >> s2;
+	if (s1[0] == '.' && s2[1] == '.' || s1[1] == '.' && s2[0] == '.') {
+		cout << "No" << endl;
 	}
-	int a_cnt_2 = 0;
-	for (int i = 0; i < a_cnt; i++) {
-		if (s[i] == 'a') a_cnt_2++;
-		else break;
-	}
-	s.erase(0, a_cnt_2);
-	if (equal(s.begin(), s.begin() + s.size()/2, s.rbegin())) {
-		cout << "Yes" << endl;
-	}
-	else cout << "No" << endl;
+	else cout << "Yes" << endl;
 	return 0;
 }
