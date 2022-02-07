06f749f4e108a7d6a0c161a74ab3f9aac82f2980
monjara <yaginumee@gmail.com>
2022-02-07 21:28:27 +0900

    238-A

--- abc/cpp/abc.cpp
+++ abc/cpp/abc.cpp
@@ -3,11 +3,9 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-	string s1, s2;
-	cin >> s1 >> s2;
-	if (s1[0] == '.' && s2[1] == '.' || s1[1] == '.' && s2[0] == '.') {
-		cout << "No" << endl;
-	}
-	else cout << "Yes" << endl;
+	int n;
+	cin >> n;
+	if (pow(2, n) > pow(n, 2)) cout << "Yes" << endl;
+	else cout << "No" << endl;
 	return 0;
 }
