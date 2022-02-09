9c6dc0a4aa1dba23a7d2a8939a085bf9d9e0afda
monjara <yaginumee@gmail.com>
2022-02-09 21:59:19 +0900

    236-A

--- abc/cpp/abc.cpp
+++ abc/cpp/abc.cpp
@@ -3,9 +3,14 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-	int n;
-	cin >> n;
-	if (pow(2, n) > pow(n, 2)) cout << "Yes" << endl;
-	else cout << "No" << endl;
+	string s;
+	int a, b;
+	cin >> s >> a >> b;
+	a--;
+	b--;
+	string s_tmp = s;
+	s[a] = s_tmp[b];
+	s[b] = s_tmp[a];
+	cout << s << endl;
 	return 0;
 }
