1a5450a7ad3a47b67c871cf1b37104b926908d7c
monjara <yaginumee@gmail.com>
2022-02-17 21:28:26 +0900

    187-A

--- abc/cpp/abc.cpp
+++ abc/cpp/abc.cpp
@@ -3,16 +3,11 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-	int n;
-	cin >> n;
-	int exp = ((1 + n) * n) * 2;
-	int sum = 0;
-	for (int i = 0; i < 4 * n - 1; i++) {
-		int tmp;
-		cin >> tmp;
-		sum += tmp;
-	}
-	int ans = exp - sum;
-	cout << ans << endl;
+	string a, b;
+	cin >> a >> b;
+	int sa = (a[0] - '0') + (a[1] - '0') + (a[2] - '0');
+	int sb = (b[0] - '0') + (b[1] - '0') + (b[2] - '0');
+	if (sa >= sb) cout << sa << endl;
+	else cout << sb << endl;
 	return 0;
 }
