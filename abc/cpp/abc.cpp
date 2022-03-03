814053b73ee2853f780aa28814b03490196aa2a0
monjara <yaginumee@gmail.com>
2022-03-03 22:52:09 +0900

    241-B

--- abc/cpp/abc.cpp
+++ abc/cpp/abc.cpp
@@ -3,8 +3,26 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-	vector<int> a(10);
-	for (int i = 0; i <= 9; i++) cin >> a.at(i);
-	cout << a.at(a.at(a.at(0))) << endl;
+	int n, m;
+	cin >> n >> m;
+	vector<int> a(n);
+	vector<int> b(m);
+	for (int i = 0; i < n; i++) cin >> a.at(i);
+	for (int i = 0; i < m; i++) cin >> b.at(i);
+	for (int i = 0; i < m; i++) {
+		bool ok = false;
+		for (int j = 0; j < n; j++) {
+			if (b.at(i) == a.at(j)) {
+				ok = true;
+				a.at(j) = 0;
+				break;
+			}
+		}
+		if (!ok) {
+			cout << "No" << endl;
+			return 0;
+		}
+	}
+	cout << "Yes" << endl;
 	return 0;
 }
