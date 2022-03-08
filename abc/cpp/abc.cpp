a0697a19002d04cbb9e495f681d107afcf6fe6f9
monjara <yaginumee@gmail.com>
2022-03-08 21:30:20 +0900

    241-B

--- abc/cpp/abc.cpp
+++ abc/cpp/abc.cpp
@@ -3,12 +3,9 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-	double a, b, c, x;
-	cin >> a >> b >> c >> x;
-	if (x <= a) cout << 1.000000 << endl;
-	else if (b < x) cout << 0.000000 << endl;
-	else {
-		cout << fixed << setprecision(10) << c / (b - a) << endl;
-	}
+	string s;
+	cin >> s;
+	sort(s.begin(), s.end());
+	cout << s << endl;
 	return 0;
 }
