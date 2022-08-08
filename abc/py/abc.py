358574a75e10bec7cdc43d72b498cf9e54539b19
monjara <yaginumee@gmail.com>
2022-08-08 20:36:41 +0900

    fibonacci

--- abc/py/abc.py
+++ abc/py/abc.py
@@ -1,8 +1,10 @@
-a, b = map(int, input().split())
+def fibonacci(n):
+	if n == 0:
+		return 1
+	elif n == 1:
+		return 1
+	else:
+		return fibonacci(n - 1) + fibonacci(n - 2)
 
-if ( 0 < a ) & ( b == 0 ):
-	print('Gold')
-elif ( a == 0 ) & ( 0 < b ):
-	print('Silver')
-elif ( 0 < a ) & ( 0 < b ):
-	print ('Alloy')
+for i in range(8):
+	print(fibonacci(i))
