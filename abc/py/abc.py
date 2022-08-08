1759b17b73962ae29852fec1c1c305f4cbf4ccc5
monjara <yaginumee@gmail.com>
2022-08-08 22:12:40 +0900

    260-A python

--- abc/py/abc.py
+++ abc/py/abc.py
@@ -1,16 +1,14 @@
-def binary_search(data, value):
-	left = 0
-	right = len(data) - 1
-	while left <= right:
-		mid = (left + right)
-		if data[mid] == value:
-			return mid
-		elif data[mid] < value:
-			left = mid + 1
-		else:
-			right = mid - 1
-	return -1
-
-data = [10, 20, 30, 40, 50, 60, 70, 80, 90]
-print(binary_search(data, 10))
-
+s = input()
+d = {}
+for i in s:
+	if i not in d:
+		d[i] = 0
+	d[i] += 1
+ok = False
+for k, v in d.items():
+	if v == 1:
+		print(k)
+		ok = True
+		break
+if ok is False:
+	print(-1)
